52be76c0c27fd32794053519be0c2bdf77d446e1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-10 22:18:23 +0200

    make a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
-#include <iostream>
+#include <raylib.h>
+
+#include "colors/Rgb.hpp"
+#include "ints.hpp"
+
+struct Grid {
+    static constexpr i32 tileSize = 32;
+    static constexpr i32 width = 30;
+    static constexpr i32 height = 20;
+};
 
 int main() {
-    std::cout << "hi" << std::endl;
+    InitWindow(Grid::width * Grid::tileSize, Grid::height * Grid::tileSize,
+               "Swoosh");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(catpuccin::Lavender.opaque());
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
